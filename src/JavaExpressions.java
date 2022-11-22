828d30c109a0848dfc479c0431c2746c493a2028
Fernando <ferg.developer@gmail.com>
2022-11-22 17:07:34 -0500

    Test Commit 1

--- src/JavaExpressions.java
+++ src/JavaExpressions.java
@@ -4,7 +4,7 @@ public class JavaExpressions {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		double friends = 20;
+		double friends = 30;
 		friends = friends + 3;
 		friends = friends - 1;
 		friends = friends * 1;
