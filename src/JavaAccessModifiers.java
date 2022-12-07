654012096f4949ec4afa99ab27f43a84993996d1
Fernando <ferg.developer@gmail.com>
2022-12-06 19:21:22 -0500

    Abstraction and access modifiers

---
+++ src/JavaAccessModifiers.java
@@ -0,0 +1,9 @@
+
+public class JavaAccessModifiers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/package2/C.java
@@ -0,0 +1,11 @@
+package package2;
+import package1.*; 
+
+public class C {
+
+	public  String publicMessage = "this is a public message";
+	protected String protectedMessage = "This is a protected message";
+			String defaultMessage = "This is the default";
+	private String privateMessage = "this is private";
+	
+}
